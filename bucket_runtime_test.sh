c1d8ff8767943a4afb5b2b846c9f71963bca55d9
mkirsche <jnredotter@gmail.com>
2018-06-11 18:56:00 -0400

    Added test for runtime based on number of buckets

---
+++ bucket_runtime_test.sh
@@ -0,0 +1,11 @@
+./build.sh
+genome=$1
+safile=$2
+saplingfile=$3
+outfile=$4
+echo '' > $outfile
+for i in `seq 5 25`;
+do
+    echo 'Buckets:' $i
+    ./sapling $1 $2 $3'_'$i $i | tee $outfile
+done 
