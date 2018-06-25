4e23a62999615280fe2e0a2f7d8866c8d4a04994
mkirsche <jnredotter@gmail.com>
2018-06-25 14:40:06 -0400

    Updated build script

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 g++ -O2 -std=c++11 -o sapling -w sapling.cpp
 g++ -O2 -std=c++11 -o binarysearch -w binarysearch.cpp
 g++ -O2 -std=c++11 -o errors -w errors.cpp
+g++ -O2 -std=c++11 -o genomelength_runtime -w genomelength_runtime.cpp
