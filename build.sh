5626e7d53dc9b943d4779f0d82ff6d1b256b020b
mkirsche <jnredotter@gmail.com>
2018-06-26 14:47:41 -0400

    Added binary search genome length test

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@ g++ -O2 -std=c++11 -o sapling -w sapling.cpp
 g++ -O2 -std=c++11 -o binarysearch -w binarysearch.cpp
 g++ -O2 -std=c++11 -o errors -w errors.cpp
 g++ -O2 -std=c++11 -o genomelength_runtime -w genomelength_runtime.cpp
+g++ -O2 -std=c++11 -o genomelength_binarysearch -w genomelength_binarysearch.cpp
 g++ -O2 -std=c++11 -o querylength_runtime -w querylength_runtime.cpp
---
+++ genomelength_binarysearch.sh
@@ -0,0 +1,15 @@
+./build.sh
+genome=$1
+safile=$2
+saplingfile=$3
+min=$4
+max=$5
+outfile=$6
+echo '' > $outfile
+for i in `seq $min $max`;
+do
+    pow=$((2**i))
+    echo 'Length:' $pow
+    echo 'Length:' $pow >> $outfile
+    ./genomelength_runtime $1 'genomelengthbinarysearch_'$2'_'$pow 'genomelengthbinarysearch_'$3'_'$pow $pow >> $outfile
+done  
