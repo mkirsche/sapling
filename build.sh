7b13d6bef7d95a2bdcba0fce09dd8f072cc84ad3
mkirsche <jnredotter@gmail.com>
2018-06-26 11:19:35 -0400

    Added c++ program for running one instance of query length runtime test

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@ g++ -O2 -std=c++11 -o sapling -w sapling.cpp
 g++ -O2 -std=c++11 -o binarysearch -w binarysearch.cpp
 g++ -O2 -std=c++11 -o errors -w errors.cpp
 g++ -O2 -std=c++11 -o genomelength_runtime -w genomelength_runtime.cpp
+g++ -O2 -std=c++11 -o querylength_runtime -w querylength_runtime.cpp
