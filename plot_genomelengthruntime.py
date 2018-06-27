6257d7f3604b1c51d5b200edf7035a563d6a45b9
mkirsche <jnredotter@gmail.com>
2018-06-27 09:18:09 -0400

    Added updated genome length runtime results

--- plot_genomelengthruntime.py
+++ plot_genomelengthruntime.py
@@ -4,18 +4,30 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import sys
 import math
+import matplotlib.patches as mpatches
+
+sapcolor = 'pink'
+bscolor = 'purple'
 
 bsfn = sys.argv[1]
 saplingfn = sys.argv[2]
 bsxs = []
 bsys = []
+
+length = 'Length: '
+bstime = 'Binary search time: '
+
 with open(bsfn) as f:
     lines = f.readlines()
+    lastx = 0
     for line in lines:
-        bsxs.append(float(line.split()[0]))
-        bsys.append(float(line.split()[1]))
+        if line.startswith(length):
+            lastx = math.log(float(line[len(length):]), 2)
+        if line.startswith(bstime):
+            bsxs.append(lastx)
+            bsys.append(1000*float(line[len(bstime):]))
 time = 'Piecewise linear time: '
-length = 'Length: '
+
 saplingxs = []
 saplingys = []
 with open(saplingfn) as f:
@@ -27,23 +39,28 @@ with open(saplingfn) as f:
         if line.startswith(time):
             saplingxs.append(lastx)
             saplingys.append(1000*float(line[len(time):]))
-print(saplingys)
+
 allxs = []
 allys = []
 colors = []
 for i in range(0, len(bsxs)):
     allxs.append(bsxs[i])
     allys.append(bsys[i])
-    colors.append('purple')
+    colors.append(bscolor)
     
 for i in range(0, len(saplingxs)):
     allxs.append(saplingxs[i])
     allys.append(saplingys[i])
-    colors.append('red')
+    colors.append(sapcolor)
     
 plt.scatter(x = allxs, y = allys, c = colors)
 
 plt.title('Genome Length Runtime')
 plt.xlabel('log(Genome Length)')
 plt.ylabel('Milliseconds per 5 million queries')
+
+sappatch = mpatches.Patch(color=sapcolor, label='SAPLING')
+bspatch = mpatches.Patch(color=bscolor, label='Binary search')
+plt.legend(handles=[sappatch, bspatch])
+
 plt.savefig(sys.argv[3])
