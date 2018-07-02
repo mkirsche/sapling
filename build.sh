0e4f3fa899e420e15e549fab88caf95a4ac9c9d1
mkirsche <jnredotter@gmail.com>
2018-07-02 12:47:30 -0400

    Added version which keeps track of maximum errors per segment

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 g++ -O2 -std=c++11 -o sapling -w sapling.cpp
+g++ -O2 -std=c++11 -o sapling_error_function -w sapling_error_function.cpp
 g++ -O2 -std=c++11 -o binarysearch -w binarysearch.cpp
 g++ -O2 -std=c++11 -o errors -w errors.cpp
 g++ -O2 -std=c++11 -o genomelength_runtime -w genomelength_runtime.cpp
