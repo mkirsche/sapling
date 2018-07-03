88f9977dcc9170c70621f9aab73f1a966f88ae05
mkirsche <jnredotter@gmail.com>
2018-07-03 13:50:54 -0400

    Decoupled API and usage example

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 g++ -O2 -std=c++11 -o sapling -w sapling.cpp
 g++ -O2 -std=c++11 -o sapling_error_function -w sapling_error_function.cpp
+g++ -O2 -std=c++11 -o sapling_example -w sapling_example.cpp
 g++ -O2 -std=c++11 -o binarysearch -w binarysearch.cpp
 g++ -O2 -std=c++11 -o errors -w errors.cpp
 g++ -O2 -std=c++11 -o genomelength_runtime -w genomelength_runtime.cpp
