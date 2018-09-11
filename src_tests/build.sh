d78be082de2b6cd72c6c39672eb9e46f24e4a73f
mkirsche <jnredotter@gmail.com>
2018-09-11 14:47:29 -0400

    Added test for different types of functions

--- src_tests/build.sh
+++ src_tests/build.sh
@@ -1,8 +1,8 @@
 g++ -O2 -std=c++11 -o sapling -w sapling.cpp
 g++ -O2 -std=c++11 -o sapling_error_function -w sapling_error_function.cpp
-g++ -O2 -std=c++11 -o sapling_example -w sapling_example.cpp
 g++ -O2 -std=c++11 -o binarysearch -w binarysearch.cpp
 g++ -O2 -std=c++11 -o errors -w errors.cpp
 g++ -O2 -std=c++11 -o genomelength_runtime -w genomelength_runtime.cpp
 g++ -O2 -std=c++11 -o genomelength_binarysearch -w genomelength_binarysearch.cpp
 g++ -O2 -std=c++11 -o querylength_runtime -w querylength_runtime.cpp
+g++ -O2 -std=c++11 -o example -w example.cpp
